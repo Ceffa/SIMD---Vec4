c48d0012bcde3bf092f71ac9be5128c0510e4d9a
Filippo Ceffa <ceffa93@gmail.com>
2018-01-14 19:52:09 +0100

    Using only intrinsics, and added some complex vector operations

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 clear;
-g++ -std=c++14 -O3 -march=native -msse2 -lm -D__NO_INLINE__ -Wall -Werror=pedantic main.cpp vec4.cpp  -o exec.exe;		
-./exec.exe;
+g++ -std=c++14 -O3 -march=native -msse4 -lm -D__NO_INLINE__ -Wall -Werror=pedantic main.cpp vec4.cpp  -o exec.exe;		
+time ./exec.exe;
 
--- vec4.h
+++ vec4.h
@@ -1,53 +1,41 @@
 #include "types.h"
 #include <iostream>
-#include <emmintrin.h>
+#include <smmintrin.h>
+#include <cmath>
 
 class vec4{
 public:
 
 	//CONSTRUCTORS
-	constexpr vec4() noexcept{}
-	constexpr vec4(F32 X, F32 Y, F32 Z, F32 W) noexcept : XYZW{X,Y,Z,W}{}
-	constexpr vec4(F32 V) noexcept : XYZW{V,V,V,V}{}
+	vec4() : M128(_mm_set_ps(0,0,0,0)){}
+	vec4(F32 X, F32 Y, F32 Z, F32 W) : M128(_mm_set_ps(W,Z,Y,X)){}
+	vec4(F32 V) : M128(_mm_set_ps(V,V,V,V)){}
 
 	//MEMBER ACCESS
-	constexpr F32 x() const noexcept {return XYZW[0];}
-	constexpr F32 y() const noexcept {return XYZW[1];}
-	constexpr F32 z() const noexcept {return XYZW[2];}
-	constexpr F32 w() const noexcept {return XYZW[3];}
+	F32 x() const {return _mm_cvtss_f32(M128);}
+	F32 y() const {return _mm_cvtss_f32(_mm_shuffle_ps(M128,M128, _MM_SHUFFLE(0, 0, 0, 1)));}
+	F32 z() const {return _mm_cvtss_f32(_mm_shuffle_ps(M128,M128, _MM_SHUFFLE(0, 0, 0, 2)));}
+	F32 w() const {return _mm_cvtss_f32(_mm_shuffle_ps(M128,M128, _MM_SHUFFLE(0, 0, 0, 3)));}
 
-	//BASE MATH		
-	vec4 operator + (const vec4& other) const{
-		return vec4(_mm_add_ps(M128, other.M128));
-	}
-	vec4 operator - (const vec4& other) const{
-		return vec4(_mm_sub_ps(M128, other.M128));
-	}
-	vec4 operator * (const vec4& other) const{
-		return vec4(_mm_mul_ps(M128, other.M128));
-	}
-	vec4 operator / (const vec4& other) const{
-		return vec4(_mm_div_ps(M128, other.M128));
-	}
-	
+	//MATH OPERATIONS	
+	vec4 operator + (const vec4& other) const {return vec4(_mm_add_ps(M128, other.M128));}
+	vec4 operator - (const vec4& other) const {return vec4(_mm_sub_ps(M128, other.M128));}
+	vec4 operator * (const vec4& other) const {return vec4(_mm_mul_ps(M128, other.M128));}
+	vec4 operator / (const vec4& other) const {return vec4(_mm_div_ps(M128, other.M128));}
 	
 	//INCREMENTS
-	vec4& operator += (const vec4& other){
-		M128 = _mm_add_ps(M128, other.M128);
-		return *this;
-	}
-	vec4& operator -= (const vec4& other){
-		M128 = _mm_sub_ps(M128, other.M128);
-		return *this;
-	}
-	vec4& operator *= (const vec4& other){
-		M128 = _mm_mul_ps(M128, other.M128);
-		return *this;
-	}
-	vec4& operator /= (const vec4& other){
-		M128 = _mm_div_ps(M128, other.M128);
-		return *this;
-	}
+	vec4& operator += (const vec4& other) {M128 = _mm_add_ps(M128, other.M128);	return *this;}
+	vec4& operator -= (const vec4& other) {M128 = _mm_sub_ps(M128, other.M128);	return *this;}
+	vec4& operator *= (const vec4& other) {M128 = _mm_mul_ps(M128, other.M128);	return *this;}
+	vec4& operator /= (const vec4& other) {M128 = _mm_div_ps(M128, other.M128);	return *this;}
+
+	//VECTOR OPERATIONS
+	F32 magnitude() const { return _mm_cvtss_f32(_mm_sqrt_ss(_mm_dp_ps(M128,M128,0xFF)));}
+	void normalize() { M128 = _mm_mul_ps(M128, _mm_rsqrt_ps(_mm_dp_ps(M128,M128,0xFF)));}
+	vec4 normalized() const {return vec4(_mm_mul_ps(M128, _mm_rsqrt_ps(_mm_dp_ps(M128,M128,0xFF))));}
+	F32 dot(const vec4& other) const {return _mm_cvtss_f32(_mm_dp_ps(M128,other.M128,0xF1));}
+	
+
 		
 	//TO STRING
 	friend std::ostream& operator<<(std::ostream& o, const vec4& v) {
@@ -58,10 +46,7 @@ public:
 
 
 private:
-	union{
-		F32 XYZW[4] = {0};
-		__m128 M128;
-	};
+	__m128 M128;
 	constexpr vec4(__m128 _M128) noexcept : M128(_M128){}
 
 
